495d38991c75cc0f394dc4fb3cee4d1840c52e08
bao <bao_java@126.com>
2017-04-08 08:53:12 +0800

    dfd

--- starjobs/src/main/java/com/starjobs/controller/SignInController.java
+++ starjobs/src/main/java/com/starjobs/controller/SignInController.java
@@ -3,11 +3,28 @@
  */
 package com.starjobs.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 /**
  * <p>Title:SignIn</p>
  * <p>Description:</p>
  * @author:bao
  */
+@Controller
 public class SignInController {
-
+	
+	@RequestMapping(value="/user/register/sign_in",method=RequestMethod.GET)
+	@ResponseBody
+	public Map<String,Object> verifyCode(){
+		Map<String,Object> modelMap = new HashMap<String,Object>(3);
+		modelMap.put("error_code", "200");
+		modelMap.put("message", "success");
+		return modelMap;
+	}
 }
