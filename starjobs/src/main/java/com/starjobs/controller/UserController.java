99aaf1566c79c765129d9af240232993ce1d5a01
taojian <1099593012@qq.com>
2017-04-09 23:09:38 +0800

    整合页面
    跳转

---
+++ starjobs/src/main/java/com/starjobs/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.starjobs.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserController {
+
+	@RequestMapping("/login")
+	public String loginAdmin(){
+		return "jsp/index";
+	}
+}
