3322035471662b8b8676dd4ff21ed9b1c9616c3e
bao <bao_java@126.com>
2017-04-10 22:37:38 +0800

    修改

--- starjobs/src/main/java/com/starjobs/service/UserService.java
+++ starjobs/src/main/java/com/starjobs/service/UserService.java
@@ -12,5 +12,9 @@ import java.util.Map;
  */
 public interface UserService {
 	//注册时验证验证码
-	public Map<String,Object> verifyCode(String phone,String code,String password,String userFlag);
+	public Map<String,Object> verifyCode(String phone,String code,String password,String userFlag,String appFlag);
+	//用户登录
+	public Map<String,Object> userLogin(String phone,String password,String userFlag);
+	//用户找回密码
+	public Map<String,Object> userRetrievePwd(String phone,String code,String password,String userFlag,String appFlag);
 }
