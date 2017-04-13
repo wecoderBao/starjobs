cbe6ce26e488be70a173ebca087d200e73322262
bao <bao_java@126.com>
2017-04-13 22:23:38 +0800

    添加ap包

---
+++ starjobs/src/main/java/com/starjobs/controller/app/UserController.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.starjobs.controller.app;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * <p>Title:UserController</p>
+ * <p>Description:app端用户查询招聘信息等控制类</p>
+ * @author:bao
+ * @date:2017年4月13日下午9:22:39
+ */
+@Controller
+public class UserController {
+
+}
