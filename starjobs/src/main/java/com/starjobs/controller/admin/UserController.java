61332efad0f9a1ca7bb32e1ecf3c47997e89bc61
bao <bao_java@126.com>
2017-04-14 08:26:15 +0800

    df

--- starjobs/src/main/java/com/starjobs/controller/admin/UserController.java
+++ starjobs/src/main/java/com/starjobs/controller/admin/UserController.java
@@ -1,4 +1,4 @@
-package com.starjobs.controller;
+package com.starjobs.controller.admin;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
