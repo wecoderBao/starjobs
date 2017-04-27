d9318184ab1668a3d55bab9989032ac645ed6fa9
bao <bao_java@126.com>
2017-04-27 10:01:21 +0800

    add

---
+++ starjobs/src/main/java/com/starjobs/service/InfoCenterService.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.starjobs.service;
+
+import java.util.Map;
+
+/**
+ * <p>Title:InfoCenterService</p>
+ * <p>Description:个人中心信息服务类</p>
+ * @author:bao
+ * @date:2017年4月25日下午9:10:19
+ */
+public interface InfoCenterService {
+	
+	//获取普通用户个人信息
+	public Map<String, Object> getUserInfo(String token,String userFlag);
+	//更新普通用户信息
+	public Map<String, Object> updateUserInfo(String token,Map<String,String> params,String path);
+}
---
+++ starjobs/src/main/java/com/starjobs/service/impl/InfoCenterServiceImpl.java
@@ -0,0 +1,144 @@
+/**
+ * 
+ */
+package com.starjobs.service.impl;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import com.starjobs.common.ImageUtil;
+import com.starjobs.mapper.TUserInfoMapper;
+import com.starjobs.mapper.TUserTokenMapper;
+import com.starjobs.pojo.TUserInfo;
+import com.starjobs.pojo.TUserToken;
+import com.starjobs.service.InfoCenterService;
+import com.starjobs.service.TokenService;
+
+/**
+ * <p>
+ * Title:InfoCenterServiceImpl
+ * </p>
+ * <p>
+ * Description:
+ * </p>
+ * 
+ * @author:bao
+ * @date:2017年4月25日下午9:11:50
+ */
+@Service
+public class InfoCenterServiceImpl implements InfoCenterService {
+
+	@Autowired
+	TokenService tokenService;
+	@Autowired
+	TUserInfoMapper tUserInfoMapper;
+	@Autowired
+	TUserTokenMapper tUserTokenMapper;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.starjobs.service.InfoCenterService#getUserInfo(java.lang.String,
+	 * java.lang.String)
+	 */
+	public Map<String, Object> getUserInfo(String token, String userFlag) {
+		// 获取手机号
+		String phoneNum = tokenService.getPhoneNum(token);
+		// 根据手机号获取个人信息
+		TUserInfo tUserInfo = tUserInfoMapper.selectByPhone(phoneNum);
+		if (tUserInfo == null) {
+			return null;
+		}
+		Map<String, Object> data = new HashMap<String, Object>();
+		data.put("token", token);
+		data.put("userFlag", userFlag);
+		data.put("headImgUrl", tUserInfo.getcUserImg());// 图片url
+		data.put("nickName", tUserInfo.getcUserNickname());
+		data.put("username", tUserInfo.getcUsername());
+		data.put("gender", tUserInfo.getcUserGender());
+		data.put("birthday", tUserInfo.getcUserBirthDate());// 出生日期
+		data.put("height", tUserInfo.getcUserHeight());
+		data.put("eduState", tUserInfo.getcUserEduState());
+		data.put("school", tUserInfo.getcUserSchoolName());
+		data.put("phoneNum", tUserInfo.getcUserPhone());
+		data.put("balance", tUserInfo.getcUserBalance());
+		return data;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.starjobs.service.InfoCenterService#updateUserInfo(java.util.Map)
+	 */
+	public Map<String, Object> updateUserInfo(String token, Map<String, String> params,String path) {
+		// 获取手机号
+		String phoneNum = tokenService.getPhoneNum(token);
+		// 根据手机号获取个人信息
+		TUserInfo tUserInfo = tUserInfoMapper.selectByPhone(phoneNum);
+		if (tUserInfo == null) {
+			return null;
+		}
+		// 若修改手机号
+		String newPhone = params.get("phoneNum");
+		if (!StringUtils.isEmpty(newPhone)) {
+			// 根据token修改手机号
+			TUserToken tokenRecord = tUserTokenMapper.selectByTokenValue(token);
+			if (tokenRecord == null) {
+				return null;
+			}
+			tokenRecord.setcPhoneNum(newPhone);
+			int re = tUserTokenMapper.updateByPrimaryKeySelective(tokenRecord);
+			if (re != 1) {
+				return null;
+			}
+			tUserInfo.setcUserPhone(newPhone);// 修改手机号
+		}
+		// 修改头像
+		String headImg = params.get("headImg");
+		headImg=headImg.replaceAll(" ", "+");//base64字符串中加号被替换成空格，这里替换回来
+		
+		String imgFormat = params.get("imgFormat");
+		
+		if (!StringUtils.isEmpty(headImg) && !StringUtils.isEmpty(ImageUtil.photoFormat(imgFormat))) {
+			String resp = ImageUtil.saveStr2Photo(path, headImg, imgFormat);
+			if (!StringUtils.isEmpty(resp)) {
+				tUserInfo.setcUserImg(resp);
+			}
+		}
+		if (!StringUtils.isEmpty(params.get("nickName"))) {// 修改昵称
+			tUserInfo.setcUserNickname(params.get("nickName"));
+		}
+		if (!StringUtils.isEmpty(params.get("username"))) {// 修改姓名
+			tUserInfo.setcUsername(params.get("username"));
+		}
+		if (!StringUtils.isEmpty(params.get("gender"))) {// 修改性别
+			tUserInfo.setcUserGender(params.get("gender"));
+		}
+		if (!StringUtils.isEmpty(params.get("birthday"))) {// 修改生日
+			tUserInfo.setcUserBirthDate(new Date());
+		}
+		if (!StringUtils.isEmpty(params.get("height"))) {// 修改身高
+			tUserInfo.setcUserHeight(params.get("height"));
+		}
+		if (!StringUtils.isEmpty(params.get("eduState"))) {// 修改教育情况
+			tUserInfo.setcUserEduState(params.get("eduState"));
+		}
+		if (!StringUtils.isEmpty(params.get("school"))) {// 修改学校名字
+			tUserInfo.setcUserSchoolName(params.get("school"));
+		}
+
+		int re = tUserInfoMapper.updateByPrimaryKeySelective(tUserInfo);
+		if (re == 1) {
+			Map<String, Object> data = new HashMap<String, Object>();
+			data.put("token", token);
+			return data;
+		}
+		return null;
+	}
+
+}
