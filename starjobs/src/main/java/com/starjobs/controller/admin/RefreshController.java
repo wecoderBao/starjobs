9c058afef10118ec2593ab3a3d3734abf3666902
sunbao <sunbao@corp.netease.com>
2018-01-10 11:34:51 +0800

    poi

--- starjobs/src/main/java/com/starjobs/controller/admin/RefreshController.java
+++ starjobs/src/main/java/com/starjobs/controller/admin/RefreshController.java
@@ -4,10 +4,13 @@ import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -17,8 +20,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.starjobs.common.ExportExcel;
 import com.starjobs.common.PageUtil;
-import com.starjobs.common.Result;
+import com.starjobs.pojo.CompanyRefresh;
 
 @Controller
 public class RefreshController {
@@ -32,10 +36,25 @@ public class RefreshController {
 	@RequestMapping("export/file")
 	public void exportFile(HttpServletRequest request, HttpServletResponse response) {
 		String docsPath = request.getSession().getServletContext().getRealPath("docs");
-		
-		
-		String fileName = "refresh"+System.currentTimeMillis()+".xls";
+
+		String fileName = "refresh" + System.currentTimeMillis() + ".xls";
 		String filePath = docsPath + File.separator + fileName;
+		ExportExcel<CompanyRefresh> ex = new ExportExcel<CompanyRefresh>();
+		String[] headers = { "区域", "公司名称", "联系电话", "详细地址", "刷新次数", "消费金额" };
+		List<CompanyRefresh> dataset = new ArrayList<CompanyRefresh>();
+		dataset.add(new CompanyRefresh("洪山区", "爱尔眼科医院", "13164618323", "洪山区12号", "6", "600"));
+		dataset.add(new CompanyRefresh("洪山区", "爱尔眼科医院", "13164618323", "洪山区12号", "6", "600"));
+		dataset.add(new CompanyRefresh("洪山区", "爱尔眼科医院", "13164618323", "洪山区12号", "6", "600"));
+		try  
+        {   
+            OutputStream out = new FileOutputStream(filePath);   
+            ex.exportExcel(headers, dataset, out);  
+            out.close();  
+        } catch (FileNotFoundException e) {  
+            e.printStackTrace();  
+        } catch (IOException e) {  
+            e.printStackTrace();  
+        }  
 		download(filePath, response);
 	}
 
@@ -66,10 +85,18 @@ public class RefreshController {
 			ex.printStackTrace();
 		}
 	}
-	
-	public Result getRefreshList(@RequestParam int page, @RequestParam int limit, HttpServletRequest request,
+
+	/**
+	 * 列表数据
+	 * @param page
+	 * @param limit
+	 * @param request
+	 * @param response
+	 * @return
+	 */
+	public void getRefreshList(@RequestParam int page, @RequestParam int limit, HttpServletRequest request,
 			HttpServletResponse response) {
-		
+
 		/**
 		 * 查询数据列表
 		 */
@@ -78,7 +105,7 @@ public class RefreshController {
 		 * 查询记录总数
 		 */
 		int totalCount = 100;
-		
+
 		PageUtil pageUtil = new PageUtil(bookList, page, limit, totalCount);
 		request.setAttribute("page", pageUtil);
 		try {
@@ -90,6 +117,5 @@ public class RefreshController {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return null;
 	}
 }
