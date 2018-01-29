c0dc8461b7a69645204d9aa2389b8751342e0123
sunbao <sunbao@corp.netease.com>
2018-01-29 11:07:43 +0800

    update

---
+++ starjobs/src/main/java/com/starjobs/controller/app/PayHistoryController.java
@@ -0,0 +1,13 @@
+package com.starjobs.controller.app;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * 充值、提现、消费相关历史记录
+ * @author sunbao
+ *
+ */
+@Controller
+public class PayHistoryController {
+
+}
